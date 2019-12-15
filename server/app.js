cfcf61da62d8ec58891ad35996a11b799f0a568e
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-12-14 20:02:31 -0500

    Moves html and css files to public

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 const app = express();
 
 app.use(express.json());
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '/../client/public'));
 app.use('/', routes);
 
 module.exports = app;
