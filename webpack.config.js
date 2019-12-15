7a60c359077d4182d5ec083904f2b8ba3dcd38a0
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-12-14 20:56:08 -0500

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './client/index.jsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'client/dist')
+    path: path.resolve(__dirname, 'client/public/dist')
   },
   module: {
     rules: [
